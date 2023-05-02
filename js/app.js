d2d0f52d41ab63126d831a0fbabda8fbffae1021
gregoryrubies <GregoryRubies@gmail.com>
2023-05-03 02:25:31 +0300

    feat: add keyboard generation

--- js/app.js
+++ js/app.js
@@ -1,2 +1,29 @@
-/*Aplication*/
-alert("Уважаемый проверяющий! Из-за переезда не успел выполнить работу. Если вас не затруднит, прошу перенести проверку на пару дней. Я обязательно успею сделать в этот срок. Спасибо за понимание!")
+import request from "./modules/getKeys.js";
+
+const keysList = {};
+
+request.onload = function() {
+  let keys = request.response;
+
+  let keyboard = document.createElement("div");
+  keyboard.className = "keyboard";
+
+  keys.forEach((v, i, a) => {
+
+    let row = document.createElement("div");
+    row.id = `r_${i}`;
+    row.className = "keyboard__row";
+
+    v.forEach((v_, i_, a_) => {
+      let idx = i_ < 10 ? `${i}0${i_}` : `${i}${i_}`;
+      let key = document.createElement("button");
+      key.id = `k_${idx}`;
+      key.className = "keyboard__btn btn";
+      key.innerText = "test";
+      keysList[idx] = key;
+      row.appendChild(key);
+    });
+    keyboard.appendChild(row);
+  })
+  document.querySelector(".wrapper").appendChild(keyboard);
+}
---
+++ js/modules/getKeys.js
@@ -0,0 +1,8 @@
+let 
+  requestURL = "http://127.0.0.1:5500/sources/key-data.json",
+  request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+export default request;
