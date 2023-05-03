3ae4c68dd737febf89fea8389470945dfd4d67a9
gregoryrubies <GregoryRubies@gmail.com>
2023-05-03 03:17:16 +0300

    feat: add getKeyValue function

--- js/app.js
+++ js/app.js
@@ -2,6 +2,41 @@ import request from "./modules/getKeys.js";
 
 const keysList = {};
 
+function transformKeyData (data, lng = "en"){
+  let newData = {};
+  for(let key in data[lng]){
+    newData[key] = data[lng][key];
+  }
+  Object.keys(data)
+    .filter(v => !["ru", "en"].includes(v))
+      .forEach(k => newData[k] = data[k]);
+  return newData;
+}
+
+function getKeyValue (data) {
+
+  let content = document.createDocumentFragment();
+
+  if (data.type = 1) {
+    let [main, ext] = [document.createElement('div'), document.createElement('div')];
+    [main.innerText, ext.innerText] = [data.main, data.ext];
+    content.appendChild(...[main, ext]);
+  } else if (data.type = 2) {
+    let letter = document.createElement('div');
+    letter.innerText = data.letter;
+    content.appendChild(letter);
+  } else if (data.type = 3) {
+    let fn = document.createElement('div');
+    fn.innerText = data.fn;
+    content.appendChild(fn);
+  } else if (data.type = 4) {
+    let sign = document.createElement('div');
+    sign.innerText = data.sign;
+    content.appendChild(sign);
+  }
+  return content;
+}
+
 request.onload = function() {
   let keys = request.response;
 
@@ -19,7 +54,7 @@ request.onload = function() {
       let key = document.createElement("button");
       key.id = `k_${idx}`;
       key.className = "keyboard__btn btn";
-      key.innerText = "test";
+      key.appendChild(getKeyValue(transformKeyData(v_)));
       keysList[idx] = key;
       row.appendChild(key);
     });
