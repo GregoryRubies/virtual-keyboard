60fc48412c9adcc79233569301128b3c2ae2c23d
gregoryrubies <GregoryRubies@gmail.com>
2023-05-04 18:40:47 +0300

    refactor: change host

--- js/modules/getKeys.js
+++ js/modules/getKeys.js
@@ -2,8 +2,8 @@
  * get data from key-data.json
  */
 let 
-  requestURL = "http://127.0.0.1:5500/sources/key-data.json",
-  //requestURL = "https://gregoryrubies.github.io/virtual-keyboard/sources/key-data.json",
+  //requestURL = "http://127.0.0.1:5500/sources/key-data.json",
+  requestURL = "https://gregoryrubies.github.io/virtual-keyboard/sources/key-data.json",
   request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'json';
